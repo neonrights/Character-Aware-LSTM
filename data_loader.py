e5f1d915f29102f2bfd6fe9aa2c1e4ebbbca49cc
neonrights <nicholasfarn@gmail.com>
2017-11-15 19:22:08 -0800

    implemented lstm, started data loader and training

---
+++ data_loader.py
@@ -0,0 +1,23 @@
+# load and clean data from data folder
+
+class Loader:
+	def __init__(self, batch_size):
+		# load data sets, clean characters
+		# preprocess data sets
+		pass
+
+	def get_vocab():
+		# perform analysis of vocabulary
+		# set character and word vocabulary
+		# set character and word ids
+		# set max word length
+		pass
+
+	def next_batch():
+		# get next batch_size elements
+		# if no remaining elements, return null
+		pass
+
+	def next_epoch():
+		# reset batch pointer to start of data set
+		pass
